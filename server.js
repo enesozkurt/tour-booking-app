2667175405bc1a842b104606ab05d770511ac934
enesokurt <enes.ozkurt4@gmail.com>
2021-08-16 23:33:34 +0300

    create and read tour by mvc

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ mongoose.connect(DB, {
     useUnifiedTopology: true,
     useFindAndModify: false
 }).then(() =>
-    console.log('DB connections successfull!'));
+    console.log('DB connections successful!'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
